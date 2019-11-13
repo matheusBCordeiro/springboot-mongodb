b73005fc4300887e011db5748a97e94c4e04edd7
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2019-11-13 00:25:47 -0300

    Update UserRepository

--- src/main/java/com/spring/mongo/resources/UserResource.java
+++ src/main/java/com/spring/mongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.spring.mongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.mongo.domain.User;
+import com.spring.mongo.dto.UserDTO;
 import com.spring.mongo.services.UserService;
 
 
@@ -20,10 +22,10 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity <List<User>> findAll(){
-		
-		List <User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+ 	public ResponseEntity<List<UserDTO>> findAll() {
+		List<User> list = service.findAll();
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 }
