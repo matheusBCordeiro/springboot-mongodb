92c2c7a28d1ea7632b9b727246fbb2be30a9b8a5
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2019-11-18 21:11:43 -0300

    POST User Insertion

--- src/main/java/com/spring/mongo/resources/exception/ResourcesExceptionAdvice.java
+++ src/main/java/com/spring/mongo/resources/exception/ResourcesExceptionAdvice.java
@@ -16,7 +16,7 @@ public class ResourcesExceptionAdvice {
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
 		
 		HttpStatus status = HttpStatus.NOT_FOUND;
-		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
-		return ResponseEntity.status(status).body(err);
+		StandardError error = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(error);
 	}
 }
--- src/main/java/com/spring/mongo/services/UserService.java
+++ src/main/java/com/spring/mongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.spring.mongo.domain.User;
+import com.spring.mongo.dto.UserDTO;
 import com.spring.mongo.repository.UserRepository;
 import com.spring.mongo.services.exception.ObjectNotFoundException;
 
@@ -22,5 +23,12 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User obj) { 
+		return repository.insert(obj);
+	}
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
