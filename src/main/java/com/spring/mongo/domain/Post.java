386733755aab3649b4c0522fade8b8691885dbf2
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2019-11-19 21:46:02 -0300

    Adding comments to posts

--- src/main/java/com/spring/mongo/domain/Post.java
+++ src/main/java/com/spring/mongo/domain/Post.java
@@ -1,29 +1,33 @@
 package com.spring.mongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.spring.mongo.dto.AuthorDTO;
+import com.spring.mongo.dto.CommentDTO;
 
 @Document
-public class Post implements Serializable{
+public class Post implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	private String id;
-	
+
 	private Date date;
 	private String title;
 	private String body;
 	private AuthorDTO author;
-	
+
+	private List<CommentDTO> comments = new ArrayList<>();
+
 	public Post() {
 	}
-
 
 	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
@@ -34,56 +38,53 @@ public class Post implements Serializable{
 		this.author = author;
 	}
 
-
 	public String getId() {
 		return id;
 	}
-
 
 	public void setId(String id) {
 		this.id = id;
 	}
 
-
 	public Date getDate() {
 		return date;
 	}
-
 
 	public void setDate(Date date) {
 		this.date = date;
 	}
 
-
 	public String getTitle() {
 		return title;
 	}
-
 
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
-
 	public String getBody() {
 		return body;
 	}
-
 
 	public void setBody(String body) {
 		this.body = body;
 	}
 
-
 	public AuthorDTO getAuthor() {
 		return author;
 	}
-
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -92,7 +93,6 @@ public class Post implements Serializable{
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
